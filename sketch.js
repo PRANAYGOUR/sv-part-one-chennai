7a83108b4f7855130e3d22026ef6e760616f0c53
PRANAYGOUR <76206607+PRANAYGOUR@users.noreply.github.com>
2021-05-28 16:57:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+/*var oxygenChhs1 , oxygenChhs1Img;
+var oxygenChhs2 , oxygenChhs2Img;
+var oxygenChhs3 , oxygenChhs3Img;
+
+function preload(){
+  //loading images for oxygen
+  
+  oxygenChhs1Img = loadImage("oxygenChhs1.png");
+  oxygenChhs2Img = loadImage("oxygenChhs2.png");
+  oxygenChhs3Img = loadImage("oxygenChhs3.png");
+
+}
+function setup(){
+createCanvas(3000,3000)
+  // creating sprite image for oxygen available in chennai hospital no.1
+oxygenChhs1 = createSprite(600, 400,150,150);
+oxygenChhs1.addImage(oxygenChhs1Img);
+
+// creating sprite image for oxygen available in chennai hospital no.2
+oxygenChhs2 = createSprite(600, 1100,150,150);
+oxygenChhs2.addImage(oxygenChhs2Img);
+
+
+// creating sprite image for oxygen available in chennai hospital no.3
+oxygenChhs3 = createSprite(600, 1800,150,150);
+oxygenChhs3.addImage(oxygenChhs3Img);
+
+}
+
+function draw(){
+  background("red")
+  drawSprites();
+}*/
